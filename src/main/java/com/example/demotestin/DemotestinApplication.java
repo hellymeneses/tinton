d0c31d3c5645eeb39490e8a121608dfd5363a2db
Helly Meneses <hellmetv@suramericana.com.co>
2024-04-12 09:36:44 -0500

    primero prueba

---
+++ src/main/java/com/example/demotestin/DemotestinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotestin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotestinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demotestin/DemotestinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotestin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
